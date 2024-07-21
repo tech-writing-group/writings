fe4bd20b9191dfdb06fdb0e2cd6c8e6ac3062069
Yejun Kim <no1expman@gmail.com>
2024-07-21 15:24:12 +0900

    Add sample draft post for blog

---
+++ blog/yejun/web-development-overview.md
@@ -0,0 +1,8 @@
+---
+slug: web-development-overview
+title: Overview of Web Development
+authors: yejun
+unlisted: true
+---
+
+Hello
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://tech-writing-group.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'tech-writing-group', // Usually your GitHub org/user name.
+  projectName: 'tech-writing-group.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
